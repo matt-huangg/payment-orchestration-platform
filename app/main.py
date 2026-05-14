7cddb4f807b3b7c0d039ce2a29dd30037cfb0abd
matt-huangg <mahuang7@gmail.com>
2026-05-13 21:45:58 -0700

    fast api health check

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# TODO: Scaffold the FastAPI application.
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="payment orchestration",
+    version="0.1.0"
+)
+
+@app.get("/health-check")
+def health_check(): 
+    return {"status" : "ok"}
